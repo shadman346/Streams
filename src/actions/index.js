86b7add277ae554b9fe74e2b59f53d5f86f6c749
shadman346 <shadmanansari346c@gmail.com>
2021-05-10 17:21:18 +0530

    deleteStream

--- src/actions/index.js
+++ src/actions/index.js
@@ -57,7 +57,8 @@ export const editStream = (id, formValues) => async (dispatch) => {
 };
 
 export const deleteStream = (id) => async (dispatch) => {
-   await streams.post(`/streams/${id}`);
+   await streams.delete(`/streams/${id}`);
 
    dispatch({ type: DELETE_STREAM, payload: id });
+   history.push('/');
 };
--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,30 +1,59 @@
 import React from 'react';
 import Modal from '../Modal';
 import history from '../../history';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { fetchStream, deleteStream } from '../../actions';
 
-const StreamDelete = () => {
-   //either start with <div> tag or <React.Fragment> tag to let know React ,that inside is JSX code
-   // but we dont want <div> tag here thats why we use others tag instead that doesn't load in DOM.
-   //React.Fragment only work as proxy inside React environment but doesnt load in actual DOM.
-   //  <React.Fragment></React.Fragment>  can be replaced by   <></>    this
-   const actions = (
-      <>
-         <button className="ui negative button">Delete</button>
-         <button className="ui button">Cancel</button>
-      </>
-   );
+class StreamDelete extends React.Component {
+   componentDidMount() {
+      this.props.fetchStream(this.props.match.params.id);
+   }
 
-   return (
-      <div>
-         StreamDelete
+   renderAction() {
+      //either start with <div> tag or <React.Fragment> tag to let know React ,that inside is JSX code
+      // but we dont want <div> tag here thats why we use others tag instead that doesn't load in DOM.
+      //React.Fragment only work as proxy inside React environment but doesnt load in actual DOM.
+      //  <React.Fragment></React.Fragment>  can be replaced by   <></>    this
+      const { id } = this.props.match.params;
+      return (
+         <>
+            <button
+               onClick={() => this.props.deleteStream(id)}
+               className="ui negative button"
+            >
+               Delete
+            </button>
+            <Link to="/" className="ui button">
+               Cancel
+            </Link>
+         </>
+      );
+   }
+
+   renderContent() {
+      if (!this.props.stream) {
+         return 'Are you sure want to delete this stream?';
+      }
+
+      return `Are you sure you want to delete the stream with title: ${this.props.stream.title}`;
+   }
+
+   render() {
+      return (
          <Modal
             title="Delete Stream"
-            content="Are you sure you want to Delete this stream?"
-            actions={actions}
+            content={this.renderContent()}
+            actions={this.renderAction()}
             onDismiss={() => history.push('/')}
          />
-      </div>
-   );
-};
+      );
+   }
+}
 
-export default StreamDelete;
+const mapStateToProps = (state, ownProps) => {
+   return { stream: state.streams[ownProps.match.params.id] };
+};
+export default connect(mapStateToProps, { fetchStream, deleteStream })(
+   StreamDelete
+);
